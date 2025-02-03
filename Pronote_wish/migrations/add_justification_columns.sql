6bb1777335af4414bf46b1ba1e4867e60fb361b4
Bnkiller <minmin74110@gmail.com>
2025-02-03 16:42:14 +0100

    Delete Pronote_wish directory

--- Pronote_wish/migrations/add_justification_columns.sql
+++
@@ -1,6 +0,0 @@
-USE pronote;
-
--- Ajouter les colonnes pour la justification
-ALTER TABLE absences 
-ADD COLUMN justification TEXT,
-ADD COLUMN justification_status ENUM('en_attente', 'acceptee', 'refusee') DEFAULT NULL;
